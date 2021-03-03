d7e5a46fdac8f08b0b7469d57b4e621a82cca741
chenqiang03 <chenqiang03@sunlands.com>
2021-03-03 16:46:45 +0800

    feat: 11

---
+++ dist/Btn/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @description
+ * @author cq
+ * @Date 2021-01-07 10:16:08
+ * @LastEditTime 2021-01-07 10:19:38
+ * @LastEditors cq
+ */
+declare const Btn: () => JSX.Element;
+export default Btn;
