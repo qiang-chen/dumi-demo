317f9349e71dd6ed34610c12e4d514f2d46a1e52
chenqiang03 <chenqiang03@sunlands.com>
2021-03-18 18:51:51 +0800

    feat: 新增all和race方法

--- src/Promise/index.tsx
+++ src/Promise/index.tsx
@@ -2,32 +2,94 @@
  * @description
  * @author cq
  * @Date 2021-01-07 10:16:08
- * @LastEditTime 2021-03-12 20:06:31
+ * @LastEditTime 2021-03-18 18:50:09
  * @LastEditors cq
  */
 
 import React from 'react';
 import MyPromise from './myPromise';
 
-const promise: any = new MyPromise((resolve: any, _reject: any) => {
+const promise: any = new MyPromise((resolve: any, reject: any) => {
   // setTimeout(() => {
   //   resolve(100);
   // }, 1000);
-  resolve(100);
+  //  setTimeout(() => {
+  //    resolve(1000);
+  //    resolve(300);
+  //  }, 3000);
 });
 
-promise
-  .then((res: any) => {
-    return res;
-  })
-  .then(
-    res => {
-      console.log(res, '链式');
-    },
-    err => {
-      console.log(err, '链式报错');
-    },
-  );
+function test(value: any) {
+  return new MyPromise((resolve: any, reject: any) => {
+    resolve(value);
+  });
+}
+
+function test1(value: any) {
+  return new Promise((resolve: any, reject: any) => {
+    reject(value);
+  });
+}
+
+function test2(value: any) {
+  return new MyPromise((resolve: any, reject: any) => {
+    setTimeout(() => {
+      reject(value);
+    }, 2000);
+  });
+}
+
+function test3(value: any) {
+  return new MyPromise((resolve: any, reject: any) => {
+    setTimeout(() => {
+      reject(value);
+    }, 1000);
+  });
+}
+
+function test4(value: any) {
+  return new MyPromise((resolve: any, reject: any) => {
+    setTimeout(() => {
+      reject(value);
+    }, 4000);
+  });
+}
+
+// promise.all([
+//   test2(2),
+//   test3(3),
+//   test(1),
+// ]).then((res: any)=>{
+//   console.log(res);
+// },(err: any)=>{
+//   console.error(err)
+// })
+
+// promise
+//   .then((res: any) => {
+//     console.log(res);
+//     return res;
+//   },
+//     err => {
+//       return err
+//     })
+//   .then(
+//     res => {
+//       console.log(res, '链式');
+//     },
+//     err => {
+//       console.log(err, '链式报错');
+//     },
+//   );
+
+promise.race([test2(2), test3(3), test4(1)]).then(
+  (res: any) => {
+    console.log(res);
+  },
+  (err: any) => {
+    console.error(err);
+  },
+);
 
 const PromiseMy = () => {
   return <h1>look</h1>;
--- src/Promise/myPromise.ts
+++ src/Promise/myPromise.ts
@@ -2,9 +2,10 @@
  * @description
  * @author cq
  * @Date 2021-03-04 16:16:24
- * @LastEditTime 2021-03-12 20:25:44
+ * @LastEditTime 2021-03-18 18:46:46
  * @LastEditors cq
  */
+let flag = false; // 加个开关  防止成功或者失败一起调用
 
 const resolvePromise = (
   promise2: any,
@@ -15,6 +16,7 @@ const resolvePromise = (
   // 在这个函数里面需要做的事情就是 调用resolve或者reject
   // 将x的值传出去
   // 但是 x不能等于promise2本身  否则报错
+
   if (promise2 == x) {
     return reject('不能等于自己本身');
   }
@@ -24,16 +26,30 @@ const resolvePromise = (
     try {
       // 防止x身上没有then方法从而报错
       let then = x.then;
-      then.apply(
+      if (flag) {
+        return;
+      }
+      flag = true;
+      then.call(
         x,
         (res: any) => {
-          resolve(res);
+          resolvePromise(promise2, res, resolve, reject);
         },
         (err: any) => {
+          let then = x.then;
+          if (flag) {
+            return;
+          }
+          flag = true;
           reject(err);
         },
       );
     } catch (error) {
+      let then = x.then;
+      if (flag) {
+        return;
+      }
+      flag = true;
       reject(error);
     }
   } else {
@@ -42,6 +58,10 @@ const resolvePromise = (
     // 可能大家会问 为什么这里不是reject
     // 原声的promise就是不管在then的第一个函数还是第二个函数return出去的值
     // 都将在外层的then第一个函数返回
+    if (flag) {
+      return;
+    }
+    flag = true;
     resolve(x);
   }
 };
@@ -82,6 +102,14 @@ class MyPromise {
   }
   // 接受两个参数  成功回调和失败回调
   then(onFulfilled: any, onRejected: any) {
+    onFulfilled =
+      typeof onFulfilled === 'function' ? onFulfilled : (data: any) => data;
+    onRejected =
+      typeof onRejected === 'function'
+        ? onRejected
+        : (err: any) => {
+            throw err;
+          };
     // then函数 都支持链式调用的  所以我们应该return 出去我们的promise
     let promise2 = new MyPromise((resolve: any, reject: any) => {
       // 解决同步的执行
@@ -123,7 +151,6 @@ class MyPromise {
       // 什么时候会是pedding呢  异步调用的时候就会触发下述的发布订阅模式
       // 当resolve在一个异步里面触发的时候 此时.then同步执行过来了就是一个pedding
       // 那么我们不应该立马触发 将其储存起来 等待resolve或者reject执行的时候在触发
-      console.log(this.state, 'then里面方法改变', 222222);
       if (this.state == 'pedding') {
         this.onSolveCallback.push(() => {
           setTimeout(() => {
@@ -156,6 +183,61 @@ class MyPromise {
       }
     });
     return promise2;
+  }
+  // values就是一个promise的数组
+  all(values: any[]) {
+    // 创造一个存放结果的promise数组
+    const allAll = new Array(values.length);
+    let resolvedCount = 0; //计状态为resolved的promise的数量
+    return new MyPromise((resolve: any, reject: any) => {
+      values.forEach((element, index) => {
+        // element 为每一个promise的对象或者是同步代码数字  将其执行
+        if (element instanceof MyPromise) {
+          element.then(
+            (res: any) => {
+              allAll[index] = res;
+              resolvedCount++;
+              if (resolvedCount == values.length) {
+                resolve(allAll);
+              }
+            },
+            (err: any) => {
+              reject(err);
+            },
+          );
+        } else {
+          resolvedCount++;
+          allAll[index] = element;
+        }
+      });
+    });
+  }
+  race(values: any[]) {
+    // 接受的也是一个promise对象  谁执行快返回谁就OK了
+    let flag = false;
+    return new MyPromise((resolve: any, reject: any) => {
+      values.forEach((element, index) => {
+        // element 为每一个promise的对象或者是同步代码数字  将其执行
+        if (element instanceof MyPromise) {
+          element.then(
+            (res: any) => {
+              if (flag) return;
+              flag = true;
+              resolve(res);
+            },
+            (err: any) => {
+              if (flag) return;
+              flag = true;
+              reject(err);
+            },
+          );
+        } else {
+          if (flag) return;
+          flag = true;
+          resolve(element);
+        }
+      });
+    });
   }
 }
 
